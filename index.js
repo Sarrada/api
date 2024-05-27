8b5cc8b4f8963ee36281239b5abb73b17b1718ae
Saradra Acharya <SaradaICT@nppf.local>
2024-05-27 16:32:52 +0600

    last update 2705

--- index.js
+++ index.js
@@ -8,19 +8,24 @@ app.use(express.json());
 app.use(cors());
 
 const categoryRoute = require('./modules/category/router');
+const userRoute = require('./modules/user/router');
 
-app.use('/category',categoryRoute);
+app.use('/category', categoryRoute);
+app.use('/user', userRoute);
 
-app.get('/', (request, response)=>{
+app.get('/', (request, response) => {
     response.send('API is running');
-}); 
+});
 
-app.listen(8000, (req, res)=>{
+app.listen(8000, (req, res) => {
     console.log('Server is running on port 8000');
     mongoose.connect(
-        "mongodb+srv://admin:admin@cluster0.ljn5m66.mongodb.net/expense?retryWrites=true&w=majority&appName=Cluster0", { useNewUrlParser: true, useUnifiedTopology: true },).then((_) => {
+        "mongodb+srv://admin:admin@cluster0.ljn5m66.mongodb.net/expense?retryWrites=true&w=majority&appName=Cluster0",
+    ).then((_) => {
         console.log('Connected to DB');
     }).catch(err => {
         console.log(err);
     });
 });
+
+// mongodb+srv://admin:admin@cluster0.po056wz.mongodb.net/databasename?retryWrites=true&w=majority&appName=Cluster0
